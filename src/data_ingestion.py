809bd56e429d5a3eea00e8f821462d752e4d30ca
shishant1984 <shishant1984@gmail.com>
2024-10-13 13:23:40 +0530

    Version 2 for code and data

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -7,7 +7,7 @@ data_file = pd.read_csv('https://raw.githubusercontent.com/araj2/customer-databa
 # print (data_file.head())
 
 data_file = data_file.iloc[:,3:] # Remove Address/Avatat and email
-data_file = data_file[data_file['Length of Membership']>3] # Keep just data with more than 3 year membership
-
+data_file = data_file[data_file['Length of Membership']>1] # Keep just data with more than 3 year membership
+data_file.drop(columns=['Avg. Session Length'],inplace=True)
 
 data_file.to_csv(os.path.join('data','customer_data.csv')) # Save processed file at data folder with name customer_data.csv.Version 1 of data.
