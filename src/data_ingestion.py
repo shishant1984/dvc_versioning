530f1f84310b26df3dcbfbd20199a9069a07efb8
shishant1984 <shishant1984@gmail.com>
2024-10-13 13:12:17 +0530

    Snapshot of code and dvc hash file

---
+++ src/data_ingestion.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+import os
+
+data_file = pd.read_csv('https://raw.githubusercontent.com/araj2/customer-database/master/Ecommerce%20Customers.csv')
+
+# print (data_file.head())
+
+data_file = data_file.iloc[:,3:] # Remove Address/Avatat and email
+data_file = data_file[data_file['Length of Membership']>3] # Keep just data with more than 3 year membership
+
+
+data_file.to_csv(os.path.join('data','customer_data.csv')) # Save processed file at data folder with name customer_data.csv.Version 1 of data.
